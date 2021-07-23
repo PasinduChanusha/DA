01d0ab7787bc69a0da789f741000501afef0da7d
LAPTOP-EJFJDD3L\pasin <2018cs164@stu.ucsc.cmb.ac.lk>
2021-07-24 01:10:56 +0530

    init

---
+++ src/Post.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './post.css'
+
+class Post extends Component{
+    render(){
+        return(
+            <div className='post'>
+                <h2>
+                    My Name
+                </h2>
+            </div>
+        );
+    }
+}
+export default Post;
