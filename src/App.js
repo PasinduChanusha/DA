f08b9b47156a1e2837363456519522fc7aa5a91c
LAPTOP-EJFJDD3L\pasin <2018cs164@stu.ucsc.cmb.ac.lk>
2021-07-24 01:39:42 +0530

    com 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 // import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
 
 const coler ={
-  color: 'yellow'
+  color: 'Brown'
 }
+
 const App = () => {
   return (
     <div className="App">
         <h1 style={coler}>Pasindu</h1>
+        <Button>Submit</Button>
     </div>
   );
 }
