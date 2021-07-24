cb2bc6fae08a3bfb0201d05ea6c7df9bb7036eae
LAPTOP-EJFJDD3L\pasin <2018cs164@stu.ucsc.cmb.ac.lk>
2021-07-24 21:43:10 +0530

    23

--- src/App.js
+++ src/App.js
@@ -3,16 +3,10 @@ import './App.css';
 import Component1 from './Component1';
 import Component2 from './Component2';
 
-const coler ={
-  color: 'Brown'
-}
 
 const App = () => {
   return (
-    <div className="App">
-        {/* <h1 style={coler}>Pasindu</h1> */}
-        <Component1/>
-        <Component2/>
+    <div>
     </div>
   );
 }
